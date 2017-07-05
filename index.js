9b432b90df81d0d5e2a2d1462f6d108f41cfeb03
Arlin Grijalba <arlin.grijalba@gmail.com>
2017-07-05 16:48:40 -0700

    Changes on portfolio page

--- index.js
+++ index.js
@@ -1,42 +1,67 @@
 $(document).ready(function() {
+  start();
 
-  $('#leftArrow')
-    .on('mouseenter', function() {
-      $(this).addClass('animated infinite bounce');
-    })
-    .on('mouseleave', function() {
-      $(this).removeClass('animated infinite bounce');
-    })
-    .on('click', function() {
-      $('#greeting').addClass('hidden');
-      $('#portfolio').removeClass('hidden');
-    });
-  $('#rightArrow')
-    .on('mouseenter', function() {
-      $(this).addClass('animated infinite bounce');
-    })
-    .on('mouseleave', function() {
-      $(this).removeClass('animated infinite bounce');
-    });
+  function start() {
+    $('#pinkDiv')
+      .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', hidePink);
+    $('#lightBlueDiv')
+      .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', hideBlue);
 
-  $('#cuteEffect')
-    .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', doSomething);
-
-  function doSomething() {
-    $('#cuteEffect').hide();
-    $('#t')
+    $('#myName')
       .on('mouseenter', function() {
         $(this).css('color', 'hotpink');
-        $(this).css('font-size', '4.5em');
+        $(this).css('font-size', '9em');
       })
       .on('mouseleave', function() {
         $(this).css('color', 'white');
-        $(this).css('font-size', '3em');
+        $(this).css('font-size', '7em');
       });
   }
+
+  function hidePink() {
+    $('#pinkDiv').hide();
+  }
+  function hideBlue() {
+    $('#lightBlueDiv').hide();
+  }
+
+  $('#leftArrow')
+    .on('mouseenter', function() {
+      $(this).addClass('animated infinite rubberBand');
+    })
+    .on('mouseleave', function() {
+      $(this).removeClass('animated infinite rubberBand');
+    })
+    .on('click', function() {
+      $(this).addClass('animated bounceOutLeft');
+    });
+
+  $('#rightArrow')
+    .on('mouseenter', function() {
+      $(this).addClass('animated infinite rubberBand');
+    })
+    .on('mouseleave', function() {
+      $(this).removeClass('animated infinite rubberBand');
+    });
 });
 
 
+// var slides = ['greeting', 'about', 'portfolio'];
+// var currentIndex = 0;
+
+// function startSlideShow() {
+//   setInterval(function(){
+//     if (currentIndex == slides.length)
+//       currentIndex = 0;
+//     changeSlide(currentIndex);
+//     currentIndex++;
+//   }, 6500);
+// }
+
+// function changeSlide(index){
+//   var currentSlide = slides[index];
+//   var currentDiv = $('#' + currentSlide);
+// }
 
 // setGreeting();
 // function setGreeting() {
