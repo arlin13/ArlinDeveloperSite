3d4dca98a5b254f3e5bb0de35518d45f2b1e5485
Arlin Grijalba <arlin.grijalba@gmail.com>
2017-07-11 23:44:25 -0700

    onclick finally works

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ $(document).ready(function() {
       $('#myName').css('font-weight', 'normal');
       $('#myName').removeClass('pointer');
     });
+    $('#circle1').on('click', function() { changeCircle(1); });
+    $('#circle2').on('click', function() { changeCircle(2); });
+    $('#circle3').on('click', function() { changeCircle(3); });
+    $('#circle4').on('click', function() { changeCircle(4); });
+    $('#circle5').on('click', function() { changeCircle(5); });
 
   function start() {
     $('body')
@@ -91,39 +96,3 @@ $(document).ready(function() {
       $(this).removeClass('animated infinite rubberBand');
     });
 });
-
-
-// var slides = ['greeting', 'about', 'portfolio'];
-// var currentIndex = 0;
-
-// function startSlideShow() {
-//   setInterval(function(){
-//     if (currentIndex == slides.length)
-//       currentIndex = 0;
-//     changeSlide(currentIndex);
-//     currentIndex++;
-//   }, 6500);
-// }
-
-// function changeSlide(index){
-//   var currentSlide = slides[index];
-//   var currentDiv = $('#' + currentSlide);
-// }
-
-// setGreeting();
-// function setGreeting() {
-//   var now = new Date();
-//   var hour = now.getHours();
-//   if (hour >= 1 && hour <= 11) {
-//     $('#greeting').text("Good morning");
-//   } else if (hour >= 12 && hour <= 16){
-//     $('#greeting').innerHTML = "Good afternoon";
-//   } else if (hour >= 17 && hour <= 19){
-//     $('#greeting').innerHTML = "Good evening";
-//   } else if (hour >= 20 && hour <= 24){
-//     $('#greeting').innerHTML = "Good night";
-//   }
-//   else{
-//     $('#greeting').innerHTML = "Hassi";
-//   }
-// }
