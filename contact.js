ac1a2649371f98616995ea0c7c4e624d609809bb
Arlin Grijalba <arlin.grijalba@gmail.com>
2017-07-19 12:49:02 -0700

    Changes in contact page

--- contact.js
+++ contact.js
@@ -2,41 +2,15 @@ $(document).ready(function() {
   start();
 
   function start() {
-    $('#lightBlueDiv')
-      .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', hideBlue);
-
-    $('.emphazis')
-      .on('mouseenter', function() {
-        $(this).css('color', 'white');
-        $(this).css('font-size', '9em');
-      })
-      .on('mouseleave', function() {
-        $(this).css('color', 'black');
-        $(this).css('font-size', '7em');
-      });
+    $('body')
+      .css('background-color', '#F06292');
   }
 
-  function hideBlue() {
-    $('#lightBlueDiv').hide();
-  }
-
-  $('#leftArrow')
-    .on('mouseenter', function() {
-      $(this).addClass('animated infinite rubberBand');
-    })
-    .on('mouseleave', function() {
-      $(this).removeClass('animated infinite rubberBand');
-    })
-    .on('click', function() {
-      $(this).addClass('animated bounceOutLeft');
-    });
-
-  $('#rightArrow')
+  $('#upArrow')
     .on('mouseenter', function() {
       $(this).addClass('animated infinite rubberBand');
     })
     .on('mouseleave', function() {
       $(this).removeClass('animated infinite rubberBand');
     });
-
 });
